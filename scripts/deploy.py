605f8d8f60fcbb559c27062706d433780fa3beae
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-05 14:30:40 +0100

    update

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,16 @@
 from brownie import accounts, config, SimpleStorage, network
-#from brownie.network import gas_price
-#from brownie.network.gas.strategies import LinearScalingStrategy
-#gas_strategy = LinearScalingStrategy("60 gwei", "70 gwei", 1.1)
-#gas_price(gas_strategy)
+
+# from brownie.network import gas_price
+# from brownie.network.gas.strategies import LinearScalingStrategy
+# gas_strategy = LinearScalingStrategy("60 gwei", "70 gwei", 1.1)
+# gas_price(gas_strategy)
 
 
 def deploy_simple_storage():
+    # account = accounts.loafd("whatever name you want")
     account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
+    # simple_storage.wait(1)
     stored_value = simple_storage.retrieve()
     print(stored_value)
     transaction = simple_storage.store(15, {"from": account})
@@ -15,12 +18,13 @@ def deploy_simple_storage():
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
 
+
 def get_account():
-    if network.show_active() == 'development':
+    if network.show_active() == "development":
         return accounts[0]
     else:
-        return accounts.add(config['wallets']['from_key'])
-        
+        return accounts.add(config["wallets"]["from_key"])
+
 
 def main():
     deploy_simple_storage()
